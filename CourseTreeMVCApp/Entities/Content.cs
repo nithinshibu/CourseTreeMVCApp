2c3faec34dfcc1314e2dba32a5cce9d840d83b93
Nithin Mathew <nithin.mathew@sysfore.com>
2024-04-24 11:21:48 +0530

    added comment related to the one to one relationship of content and category item for more understanding about how the ef core works

--- CourseTreeMVCApp/Entities/Content.cs
+++ CourseTreeMVCApp/Entities/Content.cs
@@ -19,6 +19,9 @@ namespace CourseTreeMVCApp.Entities
 
         //The content entity is having a one to one relationship with category item entity
         //Here we are establishing the referential integrity between the content entity and the category item entity
+        /* In your Content class, you have a navigation property CategoryItem of type CategoryItem. Since this is a one-to-one relationship, Entity Framework Core will look for a foreign key property based on naming conventions. By default, EF Core will look for a property named [NavigationPropertyName]Id to use as the foreign key.
+           In your case, the navigation property is CategoryItem, so EF Core looks for a property named CategoryItemId as the foreign key. That's why you see the CategoryItemId column created in your Content table when you migrate the database. 
+        */
         public CategoryItem CategoryItem { get; set; }
 
         [NotMapped]
