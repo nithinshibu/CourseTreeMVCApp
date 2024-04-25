a70210d32bfc79ba0d6fdf21f3a4be88e0453228
Nithin Mathew <nithin.mathew@sysfore.com>
2024-04-25 10:04:18 +0530

    Created the category item controller and related views

--- CourseTreeMVCApp/Entities/CategoryItem.cs
+++ CourseTreeMVCApp/Entities/CategoryItem.cs
@@ -23,6 +23,7 @@ namespace CourseTreeMVCApp.Entities
         [Display(Name = "Media Type")]
         public int MediaTypeId { get; set; }
 
+        // To make sure this doesn't affect the migration we can add the Not Mapped attribute , hence this will be ignored
         [NotMapped]
         public virtual ICollection<SelectListItem> MediaTypes { get; set; }
 
