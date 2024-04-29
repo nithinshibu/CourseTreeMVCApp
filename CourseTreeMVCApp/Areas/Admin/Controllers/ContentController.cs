d1603a921df02a3eeac0769c92555350e27528c6
Nithin Mathew <nithin.mathew@sysfore.com>
2024-04-29 11:37:43 +0530

    Content related controller and views are added

---
+++ CourseTreeMVCApp/Areas/Admin/Controllers/ContentController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CourseTreeMVCApp.Data.DbContext;
+using CourseTreeMVCApp.Entities;
+
+namespace CourseTreeMVCApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ContentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Content
+        public async Task<IActionResult> Index()
+        {
+              return _context.Content != null ? 
+                          View(await _context.Content.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Content'  is null.");
+        }
+
+        // GET: Admin/Content/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Content == null)
+            {
+                return NotFound();
+            }
+
+            var content = await _context.Content
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+
+            return View(content);
+        }
+
+        // GET: Admin/Content/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Content/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,HTMLContent,VideoLink")] Content content)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(content);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(content);
+        }
+
+        // GET: Admin/Content/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Content == null)
+            {
+                return NotFound();
+            }
+
+            var content = await _context.Content.FindAsync(id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+            return View(content);
+        }
+
+        // POST: Admin/Content/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,HTMLContent,VideoLink")] Content content)
+        {
+            if (id != content.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(content);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContentExists(content.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(content);
+        }
+
+        // GET: Admin/Content/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Content == null)
+            {
+                return NotFound();
+            }
+
+            var content = await _context.Content
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+
+            return View(content);
+        }
+
+        // POST: Admin/Content/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Content == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Content'  is null.");
+            }
+            var content = await _context.Content.FindAsync(id);
+            if (content != null)
+            {
+                _context.Content.Remove(content);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContentExists(int id)
+        {
+          return (_context.Content?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
