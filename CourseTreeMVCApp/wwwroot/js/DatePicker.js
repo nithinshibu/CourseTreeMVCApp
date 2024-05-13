261dcb45e703b3c0162193787d5ee3034bfd5fd1
Nithin Mathew <nithin.mathew@sysfore.com>
2024-05-13 12:23:37 +0530

    changes made to Datepicker.js file

--- CourseTreeMVCApp/wwwroot/js/DatePicker.js
+++ CourseTreeMVCApp/wwwroot/js/DatePicker.js
@@ -1,10 +1,16 @@
 ﻿$(function () {
-    $('.datepicker').datepicker(
-        {
-            dateFormat: 'yy-mm-dd',
-            minDate: new Date(),
-            maxDate: AddSubtractMonths(new Date(),2)
-        }
-    );
+
+    const addMonths = 2;
+    var currDate = new Date();
+    function WireUpDatePicker() {
+        $('.datepicker').datepicker(
+            {
+                dateFormat: 'yy-mm-dd',
+                minDate: currDate,
+                maxDate: AddSubtractMonths(currDate, addMonths)
+            }
+        );
+    }
+    WireUpDatePicker();
    
 });
