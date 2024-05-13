0ebb60a781d4e49c79f3fd54de9ee4b8d6c82afb
Nithin Mathew <nithin.mathew@sysfore.com>
2024-05-13 12:18:42 +0530

    Added a partial view for the date picker code

---
+++ CourseTreeMVCApp/wwwroot/js/DateFunctions.js
@@ -0,0 +1,8 @@
+﻿function AddSubtractMonths(date, numOfMonths) {
+    //month will be a value from 0 (jan) to 11 (dec)
+    var month = date.getMonth();
+    //The result will be in milliseconds
+    var milliseconds = new Date(date).setMonth(month + numOfMonths);
+    //This will return a date which is more or less than the date provided depending on the noOfMonths value
+    return new Date(milliseconds);
+}
---
+++ CourseTreeMVCApp/wwwroot/js/DatePicker.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $('.datepicker').datepicker(
+        {
+            dateFormat: 'yy-mm-dd',
+            minDate: new Date(),
+            maxDate: AddSubtractMonths(new Date(),2)
+        }
+    );
+   
+});
