0f41ab054b24081a0545fccdecf70e5a3a3cd517
Nithin Mathew <nithin.mathew@sysfore.com>
2024-05-14 12:51:24 +0530

    Added custom migration code for the aspnetusers admin role

---
+++ CourseTreeMVCApp/Migrations/20240513120928_AddAdminAccount.cs
@@ -0,0 +1,62 @@
+﻿using CourseTreeMVCApp.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Text;
+
+#nullable disable
+
+namespace CourseTreeMVCApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminAccount : Migration
+    {
+        const string ADMIN_USER_GUID = "449a6d40-99b0-4a16-b8d1-32fe71f89228";
+        const string ADMIN_ROLE_GUID = "b2c7605c-68c4-49f4-9402-a0f7e80d3b7b";
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var hasher = new PasswordHasher<ApplicationUser>();
+
+            var passwordHash = hasher.HashPassword(null, "Password100!");
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("INSERT INTO AspNetUsers(Id, UserName, NormalizedUserName,Email,EmailConfirmed,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnabled,AccessFailedCount,NormalizedEmail,PasswordHash,SecurityStamp,FirstName,LastName,Address1,Address2,PostCode)");
+            sb.AppendLine("VALUES(");
+            sb.AppendLine($"'{ADMIN_USER_GUID}'");
+            sb.AppendLine(",'admin@techtree.co.ukok'");
+            sb.AppendLine(",'ADMIN@TECHTREE.CO.UK'");
+            sb.AppendLine(",'admin@techtree.co.uk'");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(", 0");
+            sb.AppendLine(",'ADMIN@TECHTREE.CO.UK'");
+            sb.AppendLine($", '{passwordHash}'");
+            sb.AppendLine(", ''");
+            sb.AppendLine(",'Admin'");
+            sb.AppendLine(", ''");
+            sb.AppendLine(", ''");
+            sb.AppendLine(", ''");
+            sb.AppendLine(", ''");
+            sb.AppendLine(")");
+
+            migrationBuilder.Sql(sb.ToString());
+
+            migrationBuilder.Sql($"INSERT INTO AspNetRoles (Id, Name, NormalizedName) VALUES ('{ADMIN_ROLE_GUID}','Admin','ADMIN')");
+
+            migrationBuilder.Sql($"INSERT INTO AspNetUserRoles (UserId, RoleId) VALUES ('{ADMIN_USER_GUID}','{ADMIN_ROLE_GUID}')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"DELETE FROM AspNetUserRoles WHERE UserId = '{ADMIN_USER_GUID}' AND RoleId = '{ADMIN_ROLE_GUID}'");
+
+            migrationBuilder.Sql($"DELETE FROM AspNetUsers WHERE Id = '{ADMIN_USER_GUID}'");
+
+            migrationBuilder.Sql($"DELETE FROM AspNetRoles WHERE Id = '{ADMIN_ROLE_GUID}'");
+        }
+    }
+}
