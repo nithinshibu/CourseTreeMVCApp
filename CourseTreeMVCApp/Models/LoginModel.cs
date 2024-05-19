e227d55f924b7960eb7c1138d54389cce91b7888
Nithin Mathew <nithin.mathew@sysfore.com>
2024-05-19 17:05:07 +0530

    Created the partial view for modal login dialog

---
+++ CourseTreeMVCApp/Models/LoginModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseTreeMVCApp.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+
+        public string LoginInValid { get; set; }
+
+        public string LoginFailedMessage { get; set; }
+
+    }
+}
