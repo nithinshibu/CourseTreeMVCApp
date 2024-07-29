c00c4bec03801806427e7b76f8a2e0a666e7608a
Nithin Mathew <nithin.mathew@sysfore.com>
2024-07-29 17:27:24 +0530

    Create the the UserModel View Model and the UsersCategoryListModel view Model

---
+++ CourseTreeMVCApp/Areas/Admin/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CourseTreeMVCApp.Areas.Admin.Models
+{
+    //Here we are creating a view model based on AspNetUsers entity
+    public class UserModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ CourseTreeMVCApp/Areas/Admin/Models/UsersCategoryListModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CourseTreeMVCApp.Areas.Admin.Models
+{
+    public class UsersCategoryListModel
+    {
+        //This denotes the relevant category
+        public int CategoryId { get; set; }
+
+        //This will contain a collection of all the registered users saved to the system 
+        public ICollection<UserModel> Users { get; set; }
+
+        //This will contain the collection of users that have been saved to the system for a particular category
+        public ICollection<UserModel> UsersSelected { get; set; }
+    }
+}
