6fa6fa49c48ad7e892aea884f814c3f45f9ed21b
Nithin Mathew <nithin.mathew@sysfore.com>
2024-07-29 17:49:46 +0530

    Create the '_UserListViewPartial' partial view and its Parent Index view

---
+++ CourseTreeMVCApp/Comparers/CompareUsers.cs
@@ -0,0 +1,22 @@
+﻿using CourseTreeMVCApp.Areas.Admin.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CourseTreeMVCApp.Comparers
+{
+    public class CompareUsers : IEqualityComparer<UserModel>
+    {
+        public bool Equals(UserModel? x, UserModel? y)
+        {
+           if(y== null) return false;
+
+           if(x.Id== y.Id) return true;
+
+           return false;
+        }
+
+        public int GetHashCode([DisallowNull] UserModel obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+}
